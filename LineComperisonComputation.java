57e0903d1ed81ffc6bb252ef358bd7493f97188f
mukulBillore <mukulbillore88@gmail.com>
2021-12-10 23:03:24 +0530

    commit : initial commit sucussesfull

---
+++ LineComperisonComputation.java
@@ -0,0 +1,9 @@
+package blz;
+
+public class LineComperisonComputation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to program to calculate the line comparision and computation video");
+	}
+
+}
